9fec80fabcedc3254e8129968e68566eed5bd464
Camila Guerrero <mcguegi@gmail.com>
2022-01-09 13:28:34 +0100

    Added thread store crud methods

--- postgres/thread_store.go
+++ postgres/thread_store.go
@@ -35,13 +35,22 @@ func (s *ThreadStore) Threads() ([]goreddit.Thread, error) {
 }
 
 func (s *ThreadStore) CreateThread(t *goreddit.Thread) error {
-	panic("not implemented") // TODO: Implement
+	if err := s.Get(t, `INSERT INTO threads VALUES ($1, $2, $3) RETURNING *`, t.ID, t.Title, t.Description); err != nil {
+		return fmt.Errorf("error creating thread: %w", err)
+	}
+	return nil
 }
 
 func (s *ThreadStore) UpdateThread(t *goreddit.Thread) error {
-	panic("not implemented") // TODO: Implement
+	if err := s.Get(t, `UPDATE threads SET title = $1, description = $2 WHERE id = $3 RETURNING *`, t.Title, t.Description, t.ID); err != nil {
+		return fmt.Errorf("error updating thread: %w", err)
+	}
+	return nil
 }
 
 func (s *ThreadStore) DeleteThread(id uuid.UUID) error {
-	panic("not implemented") // TODO: Implement
+	if _, err := s.Exec(`DELETE FROM threads WHERE id = $1`, id); err != nil {
+		return fmt.Errorf("error deleting thread: %w", err)
+	}
+	return nil
 }
